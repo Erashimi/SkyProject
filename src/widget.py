90914c7cf9dfbcaca35dee2f6cc1d7b661871c19
Marko Polo <erashimi@yandex.ru>
2025-02-09 12:57:23 +0300

    widget status

---
+++ src/widget.py
@@ -0,0 +1,29 @@
+def mask_account_card(data: str) -> str:
+    """
+    Функция принимает строку, содержащую тип и номер карты или счета
+    и возвращает строку с замаскированным номером.
+    """
+    words = data.split()
+    type_ = words[0]
+    number = " ".join(words[1:])
+
+    if type_ in ("Visa", "MasterCard", "Maestro"):
+        return f"{type_} {get_mask_card_number(number)}"
+    elif type_ == "Счет":
+        return f"{type_} {get_mask_account(number)}"
+
+
+def get_date(date_str: str) -> str:
+    """
+    Преобразует строку даты в формат "ДД.ММ.ГГГГ".
+    """
+    try:
+        year = date_str[:4]
+        month = date_str[5:7]
+        day = date_str[8:10]
+
+        result = f"{day}.{month}.{year}"
+
+        return result
+    except IndexError as e:
+        raise ValueError(f"Невозможно преобразовать дату: {date_str}") from e
